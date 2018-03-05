e11ebf5e0f7d496dd9c020734d1ea9eaab28976e
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-03-05 17:28:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,9 +26,9 @@ HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, users.age, SUM(pledges.amount) FROM users
+"SELECT users.name, SUM(pledges.amount) FROM users
 INNER JOIN pledges ON pledges.user_id=users.id
-GROUP BY users.name ORDER BY users.name;"
+GROUP BY users.name ORDER BY SUM(pledges.amount), user name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
